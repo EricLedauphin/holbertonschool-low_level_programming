66d4cc7f2091aca3e466867ab136118c4ee85692
EricLedauphin <8694@holbertonstudents.com>
2024-02-21 10:52:28 +0000

    0-positive_or_negative.c

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -1,10 +1,10 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
-/*
-if the number is greater than 0: is positive
-if the number is 0: is zero
-if the number is less than 0: is negative
+/* 
+ * if the number is greater than 0: is positive
+ * if the number is 0: is zero
+ * if the number is less than 0: is negative 
 */
 int main(void)
 {
