f0aab1b14b7ff96e45f8bdc19a6e51fbf9594453
EricLedauphin <8694@holbertonstudents.com>
2024-02-24 17:59:31 +0000

    7-print_last_digit.c

--- functions_nested_loops/6-abs.c
+++ functions_nested_loops/6-abs.c
@@ -1,12 +1,19 @@
-#include "main.h"
+#include <stdio.h>
 /**
-* _abs - calculate the absolute value of the number from zero.
-* @i: the integer to be converted.
-* Return: absolute value of the input number.
-*/
-int _abs(int i)
+ * main - prints numbers 0-9 without using char or printf/puts,
+ * and putchar 2x max
+(*
+ * Return: 0 on success
+ */
+int main(void)
 {
-	if (i < 0)
-		i = i * -1;
-	return (i);
+	int i = '0';
+
+	while (i <= '9')
+	{
+		putchar(i);
+		i++;
+	}
+	putchar('\n');
+	return (0);
 }
