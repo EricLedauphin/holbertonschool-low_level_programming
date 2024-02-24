0fed1fb7236079af95c875539d65093cd603539c
EricLedauphin <8694@holbertonstudents.com>
2024-02-24 17:43:25 +0000

    7-print_last_digit.c

--- functions_nested_loops/7-print_last_digit.c
+++ functions_nested_loops/7-print_last_digit.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 /**
- * main - prints the alphabet in reverse.
+* main - prints the alphabet in reverse.
 (*
- * Return: 0 on success
- */
+* Return: 0 on success
+*/
 int main(void)
 {
 	char c = 'z';
