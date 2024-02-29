a7c5fdf280b58bdbdabc2644858c1f245343a790
EricLedauphin <8694@holbertonstudents.com>
2024-02-29 14:02:25 +0000

    4-print_rev.c

--- pointers_arrays_strings/4-print_rev.c
+++ pointers_arrays_strings/4-print_rev.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
- * print_rev - prints a string in reverse, followed by a new line.
- * @str: input string to print.
+ * print_rev - prints a string, followed by a new line.
+ * @s: input string to print.
  */
 void print_rev(char *s)
 {
