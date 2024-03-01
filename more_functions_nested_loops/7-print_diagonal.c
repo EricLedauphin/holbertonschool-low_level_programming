bbff4e0d1ee749cd37849423803b90ab5b45da1a
EricLedauphin <8694@holbertonstudents.com>
2024-03-01 16:36:34 +0000

    7-print_diagonal.c

--- more_functions_nested_loops/7-print_diagonal.c
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -1,23 +1,3 @@
 #include "main.h"
 
-/**
-*Where n is the number of times the character \ should be printed
-*The diagonal should end with a \n
-*
-*/
-void print_diagonal(int n)
-{
-	int l1, l2;
-
-	for (l1 = 0; l1 < n; l1++)
-	{
-		for (l2 = 0; l2 < l1; l2++)
-		{
-			_putchar(' ');
-		}
-		_putchar(92);
-		_putchar('\n');
-	}
-	if (n <= 0)
-		_putchar('\n');
-}
+7-print_diagonal.c
